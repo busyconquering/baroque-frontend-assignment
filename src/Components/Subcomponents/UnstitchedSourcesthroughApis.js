5873ec92950001b05be486ea13962ffc3db36192
MuBBaSher-YaSiN <mubbasheryasin125@gmail.com>
2023-10-20 14:54:29 +0500

    baroque-frontend-assignment

--- src/Components/Subcomponents/UnstitchedSourcesthroughApis.js
+++ src/Components/Subcomponents/UnstitchedSourcesthroughApis.js
@@ -8,23 +8,23 @@ export default function SecondHoverPage(props) {
   const [NavNumber, setNavNumber] = useState(props.navnumber);
   const prop = props.UnstitchedApisData;
 
-// Wishlist
-  const  addOnWishlist = props.addOnWishlist; 
+  // Wishlist
+  const addOnWishlist = props.addOnWishlist;
   const wishlist = props.wishlist;
 
-  let onLikeHeartColor="text-black"
-  let exist = wishlist.find((x) => x.ProductId === prop.ProductId );
-  exist?onLikeHeartColor="text-red-500":onLikeHeartColor="text-black";
-  
+  let onLikeHeartColor = "text-black"
+  let exist = wishlist.find((x) => x.ProductId === prop.ProductId);
+  exist ? onLikeHeartColor = "text-red-500" : onLikeHeartColor = "text-black";
+
 
   return (
-    <div className="flex flex-col justify-center items-center no-underline"> 
-      
-      <Link to="/ProductDetailPage" state={{data:prop}}>
+    <div className="flex flex-col justify-center items-center no-underline">
 
-      <Link >
-        <div className={`absolute text-black ml-2 mt-2 `} onClick={()=> addOnWishlist(prop)}><BsHeart className={`cursor-default text-xl ${onLikeHeartColor}`} /></div>
-      </Link>
+      <Link to="/ProductDetailPage" state={{ data: prop }}>
+
+        <Link >
+          <div className={`absolute text-black ml-2 mt-2 `} onClick={() => addOnWishlist(prop)}><BsHeart className={`cursor-default text-xl ${onLikeHeartColor}`} /></div>
+        </Link>
         <img
           className="cursor-pointer"
           src={Image}
@@ -39,9 +39,9 @@ export default function SecondHoverPage(props) {
       </Link>
       <p className="mt-3">{prop.ProductTitle}</p>
       {/* <Link to="/ProductDetailPage" className="no-underline"> */}
-        <p className="text-black font-semibold text-normal cursor-pointer hover:underline">
-          PKR. {Number(prop.ProductPrice)}
-        </p>
+      <p className="text-black font-semibold text-normal cursor-pointer hover:underline">
+        PKR. {Number(prop.ProductPrice)}
+      </p>
       {/* </Link> */}
     </div>
   );
